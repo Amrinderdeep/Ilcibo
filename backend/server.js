60bb0009505dbf5c5e6e17f69e814f6574850510
Amrinderdeep <addybhatt22@gmail.com>
2024-12-27 13:26:32 +0530

    Simplified approach 3

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express  from "express"
-import cors from 'cors'
+const cors = require('cors');
 import { connectDB } from "./config/db.js"
 import userRouter from "./routes/userRoute.js"
 import foodRouter from "./routes/foodRoute.js"
