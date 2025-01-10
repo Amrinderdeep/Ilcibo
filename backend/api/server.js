0b46f6f1e16b4f136b86a5e0ad14fb52acf19b09
Amrinderdeep <addybhatt22@gmail.com>
2025-01-10 13:52:32 +0530

    Works on localhost 2

--- backend/api/server.js
+++ backend/api/server.js
@@ -10,57 +10,16 @@ import orderModel from "../models/orderModel.js";
 import jwt from 'jsonwebtoken';
 
 // app config
+// app config
 const app = express();
 const port = process.env.PORT || 4000;
 
 // CORS configuration
 app.use(cors());
+app.use(express.json()); // Middleware to parse JSON requests
 
 // db connection
 connectDB();
-
-app.post("/api/order/placecod", async (req, res) => {
-  try {
-    console.log("Route reached");
-
-    // Save the order to the database
-    const newOrder = new orderModel({
-      items: req.body.items,
-      amount: req.body.amount,
-      address: req.body.address,
-      payment: true,
-    });
-
-    console.log(newOrder, "newOrder Backend");
-    await newOrder.save();
-
-    // Clear the user's cart
-    await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
-
-    // Generate a JWT with the order details
-    const tokenPayload = {
-      orderId: newOrder._id,
-      items: newOrder.items,
-      amount: newOrder.amount,
-      address: newOrder.address,
-      payment: newOrder.payment,
-    };
-
-    const orderToken = jwt.sign(tokenPayload, process.env.JWT_SECRET, { expiresIn: '2h' });
-    res.statusCode =200;
-    res.setHeader('Content-Type', 'application/json');
-    // Send the response
-    res.status(200).json({
-      success: true,
-      message: "Order Placed",
-      token: orderToken,
-    });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: "Error placing the order", error: error.message });
-  }
-});
 
 // API routes
 app.use("/api/user", userRouter);
@@ -77,5 +36,3 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
-
-module.exports = app;
